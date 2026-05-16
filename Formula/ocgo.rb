49ba3bcda58d4f9888fc8dacae6a68857c5ef112
Emanuel Casco <cascoemanuel@gmail.com>
2026-05-16 16:26:55 +0200

    Update ocgo to v0.2.6

--- Formula/ocgo.rb
+++ Formula/ocgo.rb
@@ -1,16 +1,16 @@
 class Ocgo < Formula
   desc "Run Claude Code through an OpenCode Go-compatible Anthropic proxy"
   homepage "https://github.com/emanuelcasco/ocgo"
-  version "0.2.5"
+  version "0.2.6"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/emanuelcasco/ocgo/releases/download/v0.2.5/ocgo_0.2.5_darwin_arm64.tar.gz"
-      sha256 "f93d0323775d0ee6af75707ac22c3702cf7725c2a5ada759ba75bc797859be5a"
+      url "https://github.com/emanuelcasco/ocgo/releases/download/v0.2.6/ocgo_0.2.6_darwin_arm64.tar.gz"
+      sha256 "c13ff5b27ea5651d03942c443b5b4051035d5782d318758f703aae7c8f9bfa63"
     else
-      url "https://github.com/emanuelcasco/ocgo/releases/download/v0.2.5/ocgo_0.2.5_darwin_x86_64.tar.gz"
-      sha256 "0623d36a642d0e27538d35d4ae0ef4f9084d477b26d74bad30839f4bfe681ccd"
+      url "https://github.com/emanuelcasco/ocgo/releases/download/v0.2.6/ocgo_0.2.6_darwin_x86_64.tar.gz"
+      sha256 "6b01aca93805be3baf580e729624fcd4ca04eb02615337b3706cda6a4c599ae4"
     end
   end
 
